3e307d135bd9f98e364f0e50f8b7c3714905e6e8
a1bbz <albert.duhanaj@student.um.si>
2022-05-15 10:22:40 +0200

    Ustvaril polje obrazov in polje label

--- ORV/trainer.py
+++ ORV/trainer.py
@@ -1,4 +1,7 @@
 import os
+from PIL import Image
+import numpy as np
+import pickle
 import cv2
 
 face_class = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
@@ -12,10 +15,29 @@ label_array = []
 counter = 0
 label_ids = {}
 
+# Zanka, da pridemo do vseh slik v mapi
 for root, dirs, files in os.walk(IMAGE_DIRECTORY):
     for file in files:
         if file.endswith("png") or file.endswith("jpg"):
             path = os.path.join(root, file)  # Pot do slike
             label = os.path.basename(root).lower()  # ime
-            print(path)
-            print(label)
+
+            # tabela label
+            if label not in label_ids:
+                label_ids[label] = counter
+                counter = counter + 1
+            label_id = label_ids[label]
+
+            # Odpremo sliko iz poti in convertamo v sivo
+            image = Image.open(path).convert("L")
+            image_array = np.array(image, "uint8")
+
+            # Iz slike zaznamo obraz nato pa shranimo v polje
+            face = face_class.detectMultiScale(image_array, scaleFactor=1.5, minNeighbors=5)
+            for x, y, w, h in face:
+                roi = image_array[y:y + h, x:x + w]
+                trainer_array.append(roi)
+                label_array.append(label_id)
+
+print(trainer_array)
+print(label_array)
