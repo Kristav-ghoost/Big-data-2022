243ac14d167cdda6f825ad5c0b565b179cb65181
jostv23 <jost.vedlin@student.um.si>
2022-05-16 12:47:03 +0200

    Razpoznava osebe iz staticno podane slike

--- ORV/main.py
+++ ORV/main.py
@@ -1,14 +1,26 @@
 import cv2
+import pickle
 
 face_class = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
+model = cv2.face_LBPHFaceRecognizer.create(radius=1, neighbors=8, grid_x=8, grid_y=8)
+model.read("trainer.yml")
+
+# Iz indexov label dobim imena
+with open("label.pickle", "rb") as f:
+    old_labels = pickle.load(f)
+    labels = {v: k for k, v in old_labels.items()}
+
+# Testna slika
 image = cv2.imread("testImage.jpg")
 
-roi = []
+# Zaznaj obraz
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 face = face_class.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+roi = []
 for x, y, w, h in face:
     roi = gray[y:y+h, x:x+w]
 
-cv2.imshow("Found Face", roi)
-cv2.waitKey(0)
+# Prediction
+id_, conf = model.predict(roi)
+print(labels[id_])
